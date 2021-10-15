8a8bbdb756bcef0cc5e78dd721db285c39225d65
Michaela Nemcova <minkanemcova11@gmail.com>
2021-10-15 20:37:28 +0200

    feature(SB-41): Add temporary image to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import img_placeholder from './brewery_placeholder.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            {/*<h1>SmartBrew</h1>*/}
+            <img src={img_placeholder}  className="App-placeholder" alt="placeholder" />
+        </header>
+        <a href="https://www.freepik.com/vectors/vintage" className="App-credits">Vintage vector created by dgim-studio - www.freepik.com</a>
     </div>
   );
 }
