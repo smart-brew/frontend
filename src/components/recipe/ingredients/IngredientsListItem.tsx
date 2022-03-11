428d932f9d181e9b257870de8bef2f02265afb97
Michaela Nemcova <minkanemcova11@gmail.com>
2022-03-11 21:29:30 +0100

    feature(SB-197): added formatting to start brewing popup\n\nNeed to format the buttons.

--- src/components/recipe/ingredients/IngredientsListItem.tsx
+++ src/components/recipe/ingredients/IngredientsListItem.tsx
@@ -11,10 +11,12 @@ export const IngredientsListItem: React.FC<IngredientItemsProps> = ({
   showUnloader,
 }: IngredientItemsProps) => {
   return (
-    <div className="flex flex-row">
-      <div className="w-1/12 px-8 text-right">{ingredient.amount}</div>
-      <div className="w-1/12 px-4 text-left italic">{ingredient.units}</div>
-      <div className="w-max px-4 text-left">{ingredient.name}</div>
+    <div className="flex flex-row text-xl justify-between mr-6">
+      <div className="flex flex-row space-x-5">
+        <div className="w-1/12 px-8 text-right">{ingredient.amount}</div>
+        <div className="w-1/12 px-4 text-left italic">{ingredient.units}</div>
+        <div className="w-10/12 px-4 text-left">{ingredient.name}</div>
+      </div>
       {showUnloader && <div className="w-auto px-4 text-left">nasypnik </div>}
     </div>
   );
