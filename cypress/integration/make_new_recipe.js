5fed3059e443c7e6909ef76d769f3cbe07507c5e
Marek Vajda <marek.vajda009@gmail.com>
2022-04-11 14:32:26 +0200

    fix(SB-263): adding comments

--- cypress/integration/make_new_recipe.js
+++
@@ -1,6 +0,0 @@
-describe('Making new test recipe', () => {
-  it('Goes to recipe site', () => {
-    cy.visit('http://localhost:3000/recipe');
-    expect(true).to.equal(true);
-  });
-});
--- cypress/integration/start_recipe.js
+++ cypress/integration/start_recipe.js
@@ -1,13 +1,16 @@
 describe('Starting new recipe test', () => {
   it('Goes to recipe site', () => {
+    // go to /recipe site from default baseUrl in cypress.json
     cy.visit('recipe');
     cy.get('.chamber:nth-child(2)').click();
+    // select button with text
     cy.get('button').contains('Load recipe', { timeout: 10000 }).click();
     // wait for data otherwise test crashes
     cy.intercept('/api/data').as('data');
     cy.wait('@data');
     cy.get('button').contains('Start brewing', { timeout: 10000 }).click();
     cy.get('button').contains('Confirm', { timeout: 10000 }).click();
+    // arrow must be visible to pass test
     cy.get('[alt="arrow"]').should('be.visible');
   });
 });
