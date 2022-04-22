1576b978eb03e41f84c1bf69f152050eb15903ed
luknov <luckyonethe@gmail.com>
2022-04-22 17:39:25 +0200

    feature(SB-266): Added units to instructions

--- src/components/brew/BrewInstructionHistoryItem.tsx
+++ src/components/brew/BrewInstructionHistoryItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useInstructionsContext } from '../../contexts/instructionsContext';
 import { InstructionLogApi } from '../../types/BrewingType';
 import InstructionType from '../../types/RecipeData/InstructionType';
+import TimeHelper from '../../helpers/TimeHelper';
 
 interface Props {
   instructionsRecipe: InstructionType[] | null;
@@ -15,6 +17,11 @@ const BrewHistoryInstructionListItem: React.FC<Props> = ({
   const time = `${new Date(startedAt).getMinutes()}:${String(
     new Date(startedAt).getSeconds()
   ).padStart(2, '0')}`;
+
+  const { data: templates } = useInstructionsContext();
+
+  let units: string | null;
+
   return (
     <div
       className={`border rounded-xl space-y-2 py-5 px-10 ${
@@ -29,12 +36,24 @@ const BrewHistoryInstructionListItem: React.FC<Props> = ({
       }}
     >
       {instructionsRecipe?.map((instructionRecipe: InstructionType) => {
+        templates.forEach((template) => {
+          if (template.id === instructionRecipe.templateId) {
+            units = template.units;
+          }
+        });
         if (instructionId === instructionRecipe.id)
           return (
-            <div>
+            <div style={{ whiteSpace: 'nowrap' }}>
               {instructionRecipe.codeName.replaceAll('_', ' ')}{' '}
-              {instructionRecipe.param} <br />{' '}
-              {instructionRecipe.optionCodeName}{' '}
+              {instructionRecipe.codeName === 'WAIT'
+                ? `${TimeHelper.getMinutesFromMillis(
+                    instructionRecipe.param ? instructionRecipe.param : 'a'
+                  ).toLocaleString('en-US', {
+                    minimumIntegerDigits: 1,
+                    useGrouping: false,
+                  })}`
+                : instructionRecipe.param}{' '}
+              {units} <br /> {instructionRecipe.optionCodeName}{' '}
             </div>
           );
         return null;
