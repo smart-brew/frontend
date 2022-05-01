9ac16ad4670a5dde7fe8bcbc66087d1bc8f939bd
luknov <luckyonethe@gmail.com>
2022-05-01 22:17:11 +0200

    feature(SB-287): Export PDF from history stats page

--- src/pages/HistoryOverviewPage.tsx
+++ src/pages/HistoryOverviewPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 import { getBrew } from '../api/brew';
 import Menu, { MENU_HEIGHT } from '../components/menu/MenuContainer';
 import RecipePreview from '../components/recipe/RecipePreview';
@@ -31,6 +33,23 @@ export const HistoryOverview: React.FC<Props> = ({ brewId }) => {
     f();
   }, [brewId]);
 
+  const printRef = React.useRef(null);
+
+  const handleDownloadPdf = async (): Promise<void> => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element!);
+    const data = canvas.toDataURL('image/png');
+
+    /* eslint new-cap: ["error", { "newIsCap": false }] */
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    pdf.save('recipe_history.pdf');
+  };
+
   return (
     <div className="h-full">
       <Menu menus={menus} matchPathnameExact />
@@ -44,9 +63,21 @@ export const HistoryOverview: React.FC<Props> = ({ brewId }) => {
           <RecipePreview recipe={selectedBrew?.recipe ?? null} />
         </Route>
         <Route path="/history/stats" exact>
-          <BrewHistoryHeading brew={selectedBrew} />
-          {selectedBrew && <BrewHistoryInstructionList brew={selectedBrew} />}
-          <HistoryOverviewStatsPage selectedBrew={selectedBrew} />
+          <div ref={printRef}>
+            <BrewHistoryHeading brew={selectedBrew} />
+            {selectedBrew && <BrewHistoryInstructionList brew={selectedBrew} />}
+            <HistoryOverviewStatsPage selectedBrew={selectedBrew} />
+          </div>
+          <button
+            type="button"
+            onClick={handleDownloadPdf}
+            className="chamber p-2 mx-7 my-1 content-center shadow rounded-3xl items-center justify-start"
+            style={{
+              background: 'rgba(254, 208, 8, 0.1)',
+            }}
+          >
+            <div className="pl-2 font-bold">Download as PDF</div>
+          </button>
         </Route>
       </div>
     </div>
